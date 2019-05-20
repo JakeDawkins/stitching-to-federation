0eb424c4e695133cd3ee25a4eb5d506a60c9c7cf
Jake <dawkinsjh@gmail.com>
2019-05-20 14:00:44 -0400

    simplify stitching code

--- stitching-gateway/server.js
+++ stitching-gateway/server.js
@@ -1,5 +1,68 @@
 const { ApolloServer, gql } = require('apollo-server');
-const generateAndMergeSchemas = require('./remote-schemas');
+const { HttpLink } = require('apollo-link-http');
+const { setContext } = require('apollo-link-context');
+const { omit } = require('lodash');
+const fetch = require('node-fetch');
+const {
+  introspectSchema,
+  makeRemoteExecutableSchema,
+  mergeSchemas,
+} = require('graphql-tools');
+
+const reservationLink = new HttpLink({ uri: 'http://localhost:4001', fetch });
+
+const generateReservationSchema = async () => {
+  const schema = await introspectSchema(reservationLink);
+  const executableSchema = makeRemoteExecutableSchema({
+    schema,
+    link: reservationLink,
+  });
+  return executableSchema;
+};
+
+const userLink = new HttpLink({ uri: 'http://localhost:4002', fetch });
+const generateUserSchema = async () => {
+  const schema = await introspectSchema(userLink);
+  const executableSchema = makeRemoteExecutableSchema({
+    schema,
+    link: userLink,
+  });
+  return executableSchema;
+};
+
+const linkTypeDefs = `
+  extend type Reservation {
+    user: User
+  }
+`;
+
+const generateAndMergeSchemas = async () => {
+  const resSchema = await generateReservationSchema();
+  const userSchema = await generateUserSchema();
+
+  return mergeSchemas({
+    schemas: [resSchema, userSchema, linkTypeDefs],
+    resolvers: {
+      Reservation: {
+        user: {
+          fragment: `... on Reservation { userId }`,
+          resolve: (parent, args, context, info) => {
+            return info.mergeInfo.delegateToSchema({
+              schema: userSchema,
+              operation: 'query',
+              fieldName: 'user',
+              args: {
+                id: parent.userId,
+              },
+              context,
+              info,
+            });
+          },
+        },
+      },
+    },
+  });
+};
 
 generateAndMergeSchemas().then(schema => {
   const server = new ApolloServer({
